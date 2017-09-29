3d45e7aca58e860b95851a2419371dc355cfd71c
Jay Hamilton <jaydaro@gmail.com>
2017-09-29 14:33:31 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+function getFibonacci(value = null) {
+  if (!value) {
+    return [];
+  }
+
+  let fibArray = [];
+  let val1 = 0, val2 = 1, count = 0, currentVal = 0;
+  while(count < Math.abs(value + 1)) {
+    count++;
+    if (count < 3) {
+      currentVal = count - 1;
+      fibArray.push(currentVal);
+    } else {
+      currentVal = val1 + val2;
+      val1 = val2;
+      val2 = currentVal;
+      fibArray.push(currentVal);
+    }
+  }
+  return [currentVal, fibArray];
+}
+
+module.exports = { getFibonacci };
